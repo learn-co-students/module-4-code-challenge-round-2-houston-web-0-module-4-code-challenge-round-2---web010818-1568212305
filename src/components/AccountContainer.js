6036fd7ac5211ccefd34a9475a2cf479d2d19838
abideen11 <abideenkareem17@gmail.com>
2019-09-11 11:02:50 -0500

    x

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -12,18 +12,44 @@ class AccountContainer extends Component {
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
+    this.state = {
+      acct: transactions,
+      displayAcct: transactions,
+      queryAcct: ""
+    }
+
   }
 
-  handleChange(event) {
+  componentDidMount() {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+    .then(r => r.json())
+    .then(getTrans => {
+      this.setState({
+        acct: getTrans,
+        displayAcct: getTrans
+      })
+    })
+  }
+
+  handleChange = (event) => {
     // your code here
+    this.setState({
+      queryAcct: event.target.value
+    })
+  }
+
+  showResult = () => {
+    this.setState({
+      displayAcct: this.state.displayAcct.filter(a => (a.description === a.description.includes(this.state.queryAcct))  || (a.category === a.category.includes(this.state.queryAcct)))
+    })
   }
 
   render() {
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} showResult={this.showResult}/>
+        <TransactionsList displayAcct={this.state.displayAcct} />
       </div>
     )
   }
